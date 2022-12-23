fe1c0ed53b6af99fe31e21762107fbc6cbee081c
DISHANT PATEL <dishantpatel899@gmail.com>
2022-12-23 17:06:14 -0400

    Added php backend server

---
+++ restapi_backend/index.php
@@ -0,0 +1,83 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+include 'DbConnect.php';
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch($method) {
+    case "GET":
+        $sql = "SELECT * FROM users";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        if(isset($path[3]) && is_numeric($path[3])) {
+            $sql .= " WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        echo json_encode($users);
+        break;
+    case "POST":
+        $user = json_decode( file_get_contents('php://input') );
+        $sql = "INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PHONE, IMAGE_URL) VALUES(:FIRST_NAME, :LAST_NAME, :EMAIL, :PHONE, :IMAGE_URL)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':FIRST_NAME', $user->FIRST_NAME);
+        $stmt->bindParam(':LAST_NAME', $user->LAST_NAME);
+        $stmt->bindParam(':EMAIL', $user->EMAIL);
+        $stmt->bindParam(':PHONE', $user->PHONE);
+        $stmt->bindParam(':IMAGE_URL', $IMAGE_URL);
+
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record created successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to create record.'];
+        }
+        echo json_encode($response);
+        break;
+
+    case "PUT":
+        $user = json_decode( file_get_contents('php://input') );
+        $sql = "UPDATE users SET FIRST_NAME= :FIRST_NAME, LAST_NAME= :LAST_NAME, EMAIL =:EMAIL, PHONE =:PHONE, IMAGE_URL =:IMAGE_URL WHERE id = :id";
+        $stmt = $conn->prepare($sql);
+        $updated_at = date('Y-m-d');
+        $stmt->bindParam(':id', $user->id);
+        $stmt->bindParam(':FIRST_NAME', $user->FIRST_NAME);
+        $stmt->bindParam(':LAST_NAME', $user->LAST_NAME);
+        $stmt->bindParam(':EMAIL', $user->EMAIL);
+        $stmt->bindParam(':PHONE', $user->PHONE);
+        $stmt->bindParam(':IMAGE_URL', $IMAGE_URL);
+
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to update record.'];
+        }
+        echo json_encode($response);
+        break;
+
+    case "DELETE":
+        $sql = "DELETE FROM users WHERE id = :id";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $path[3]);
+
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to delete record.'];
+        }
+        echo json_encode($response);
+        break;
+}
